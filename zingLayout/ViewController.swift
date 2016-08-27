b84a1acc71174770c6ab4bb05fc45981a5676fe7
Pham Thanh <mrdark_9x@yahoo.com>
2016-08-27 16:41:03 +0700

    Initial Commit

---
+++ zingLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zingLayout
+//
+//  Created by TTB10912 on 27/08/2016.
+//  Copyright © 2016 TTB10912. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
