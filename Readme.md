9b126d23438f1efb7663b3bb8f22397d458ce194
TTB10912 <ttb10912@TTB10912s-MacBook-Pro.local>
2016-08-30 09:44:17 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Zinglayout by Pham Dinh Thanh
--- zingLayout/ViewController.swift
+++ zingLayout/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var text: UILabel!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        logo!.alpha = 0
+        
+        text!.alpha = 0
+        
+        name!.alpha = 0
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        UIView.animateWithDuration(4, animations: { self.logo!.alpha = 1 })
+            
+            
+        { (finished) in
+            
+            UIView.animateWithDuration(3, animations: {
+                
+                self.text!.center = CGPointMake(self.logo!.center.x, 100)
+                
+                self.text!.alpha = 1
+                
+            }) { (finished) in
+            
+            UIView.animateWithDuration(3, animations: {
+            
+            self.name!.center = CGPointMake(self.logo!.center.x, 500)
+            
+            self.name!.alpha = 1
+            
+            })
+                
+                
+            }
+            
+        }
+        
     }
-
-
+    
 }
 
